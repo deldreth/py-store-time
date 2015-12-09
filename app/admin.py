0c4de4c6051010a1f3aab97d7f2c3fb0d8d23ff8
Devin Eldreth <devin.eldreth@gmail.com>
2015-12-08 21:33:21 -0500

    CSV Import

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import Queue, History
 
+
+class HistoryResource(resources.ModelResource):
+
+    class Meta:
+        model = History
+        fields = ('id', 'user', 'date', 'amount')
+
+
+class HistoryAdmin(ImportExportModelAdmin):
+    model = History
+    resource_class = HistoryResource
+    list_filter = ('user',)
+
+    list_display = ('id', 'user', 'date', 'amount')
+
 admin.site.register(Queue)
-admin.site.register(History)
+admin.site.register(History, HistoryAdmin)
